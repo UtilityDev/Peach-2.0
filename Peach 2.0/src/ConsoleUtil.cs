42df59979a78529244ec341d4115d6476e53f0fa
Julian ⚡ <45518053+UtilityDev@users.noreply.github.com>
2023-12-29 12:31:41 +0100

    Add "ConsoleUtil" class and intuitive "GetStringInput" method for easier input handling.

---
+++ Peach 2.0/src/ConsoleUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Peach;
+
+public static class ConsoleUtil
+{
+    public static string GetStringInput(string msg)
+    {
+        msg = msg.ToUpper();
+        Console.Write(msg + ": ");
+
+        return Console.ReadLine();
+    }
+}
